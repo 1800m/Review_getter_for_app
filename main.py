12ece6b4b768c7cdd0603dd87d19b7bcb0f77da2
Chihiro Miyamoto <m-chihiro@dbms.cs.kumamoto-u.ac.jp>
2016-10-25 07:28:56 +0900

    modification
    python3 → python2

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # coding: UTF-8
 
-import urllib.request   # python3じゃないと動かない
+# python2版
+import urllib2
 from bs4 import BeautifulSoup
 
 # RSSを取得する
 rss_url = "https://itunes.apple.com/us/rss/newapplications/limit=100/xml"
-response = urllib.request.urlopen(rss_url)
+rss_request = urllib2.Request(rss_url)
+response = urllib2.urlopen(rss_request)
 rss = response.read().decode("utf-8")
 
 # RSSからデータを抽出する
 soup = BeautifulSoup(rss, "html.parser")
 for entry in soup.find_all("entry"):
     # タイトル
-    print(entry.find("title").string)
+    print(entry.find("title").string.encode('utf-8'))
     # リンク先URL
-    print(entry.find("id").string)
+    print(entry.find("id").string.encode('utf-8'))
     # 視聴URL
     links = [link for link in entry.find_all("link") if link["type"] == "audio/x-m4a"]
     if len(links) > 0:
